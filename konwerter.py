8b3d3175f29e62e2d7c9b941a48c9dda067d6b1a
maleksander <aleksander.maciek@gmail.com>
2015-10-24 21:31:52 +0200

    bed do fixed dziala na wielu setach danych

--- konwerter.py
+++ konwerter.py
@@ -7,7 +7,7 @@ if len(sys.argv) <2:
 
 #sprawdzenie czy wskazany plik ma wlasciwy format
 nazwa = sys.argv[1].split('.')
-if nazwa[1] != "bedGraph" and nazwa[1] != "wig":
+if nazwa[-1] != "bedGraph" and nazwa[1] != "wig":
     print "niewlasciwy format pliku"
     sys.exit(1)
 
@@ -21,103 +21,151 @@ if nazwa[1] == "bedGraph":
 elif nazwa[1] == "wig":
     nazwa[1] = "bedGraph"
 nowanazwa = ".".join(nazwa)
-
 plikzapis= open(nowanazwa, 'w')
+
 
 #czytanie linijka po linijce
 linijki =[]
 linijki = plik.readlines()
+indeksyt =[]
+indeksyf =[]
+indeksyv =[]
+track=[]
+ktora = []
+z=-1
+kolejnetracki =[]
 for i in range(0, len(linijki)):
-    #print linijki[i]
-    
-#znalezienie track line i definition line 
+
+    #znalezienie track line i definition line 
     if linijki[i].startswith("track"):
-        track = linijki[i]         
-        x = i + 1
+        z += 1
+        t = i
+        linijka = linijki[i]
+        indeksyt.append(t)
+        track.append(linijki[t])
+        ktora.append(z)
+        track_spl = linijka.split()
+        if track_spl[2] == "type=bedGraph":
+            track_spl[2] = track_spl[2].replace("type=bedGraph", "type=wiggle_0")
+        else:
+            track_spl[2] = track_spl[2].replace("type=wiggle_0", "type=bedGraph")
+        track_spl = " ".join(track_spl) + "\n"
+        kolejnetracki.append(track_spl)
+        
     elif linijki[i].startswith("fixedStep"):
         jakiformat = "fixedStep"
-        declaration_line_f = linijki[i]
-        f = i + 1
+        f = i
+        indeksyf.append(f)
     elif linijki[i].startswith("variableStep"):
         jakiformat = "variableStep"
-        v = i + 1
+        v = i
+        indeksyv.append(v)
+        
+indeksyt.append(len(linijki))
+#print indeksyt
+#print ktora
+liczba_setow = len(ktora)
 
-# zamaiana typu w track line
-track_line = track.split()  #['track', 'type=bedGraph', 'name="BedGraph', 'Format"', 'description="BedGraph', 'format"']
-if track_line[1] == "type=bedGraph":
-    track_line[1] = track_line[1].replace("type=bedGraph", "type=wiggle_0")
-else:
-    track_line[1] = track_line[1].replace("type=wiggle_0", "type=bedGraph")
-#print track_line
-track_line_def = " ".join(track_line) + "\n"
-plikzapis.write(track_line_def)
+#for i in range(0, liczba_setow):
+#    n= ktora[i]
+#    track_spl = track[n].split()
+#    if track_spl[2] == "type=bedGraph":
+#        track_spl[2] = track_spl[2].replace("type=bedGraph", "type=wiggle_0")
+#    else:
+#        track_spl[2] = track_spl[2].replace("type=wiggle_0", "type=bedGraph")
+#    track_spl = " ".join(track_spl) + "\n"
+#    kolejnetracki.append(track_spl)
+#print kolejnetracki
 
-
-
-#czytanie wartosci danych  z bedgraph
+#czytanie z bedGraph
 if jakiformat == "bedGraph":
-    chromosomy=[]
-    pozstart=[]
-    pozstop =[]
-    value=[]
-
-    for a in range (x, len(linijki)):      #czy tu nie powinno byc -1??????
-        wartosci = (linijki[a].split())
-        chromosom = wartosci[0]
-        chromosomy.append(wartosci[0])
-        start = int(wartosci[1]) + 1
-        pozstart.append(start)
-        stop = int(wartosci[2]) + 1
-        pozstop.append(stop)
-        wartosc = float(wartosci[3])
-        value.append(wartosc)
-
-#tworzenie declaration line
-
-    span = pozstop[1] - pozstart[1]
-    step = pozstop[1] - pozstop[0]
-    declaration_line=[]
-    declaration_line = ["fixedStep", "chrom="+str(chromosomy[1]),"start="+str(pozstart[0]), "step="+str(step), "span="+str(span)]
-    deklaracja = " ".join(declaration_line) + "\n"
-    #print deklaracja
-    plikzapis.write(deklaracja)
-
-
-#drukowanie wartosci w formacie fixedStep
-    for d in range(0,len(linijki)- x):  
-        #a = str(pozstart[d])
-        b = str(value[d]) + "\n"
-        #c = a +" " + b + "\n"
-        plikzapis.write(b)
-    plikzapis.close()
-
-#jesli wprowadzony plik ma format wig - fixedStep
-elif jakiformat == "fixedStep":
-    #odczyt danych z fixedstep
-    import re
-    DATA = declaration_line_f
-    declaration_line_f = re.findall(r"[\w']+", DATA)
-    chrom = declaration_line_f[2]
-    st = int(declaration_line_f[4]) -1
-    stp = int(declaration_line_f[6])
-    spn = int(declaration_line_f[8])
-    valu=[]
-    for e in range(f, len(linijki)): #powinno byc -1, dlaczego nie wyswietla ostatniego gdy jest -1???????
-        val= str(linijki[e])
-        valu.append(val)
-
-    #drukowanie wartosci w formacie BED
-    for d in range(0,len(linijki)- f):
-        lista = str(chrom) + " " + str(st) + " " + str(st + spn) + " " + str(valu[d])
-        st += spn
-        plikzapis.write(lista)
-    plikzapis.close()
+    a=-1
+    b=a+1
+    for j in range(0, len(indeksyt)-1):
+        a+=1
+        b+=1        
+        sekcja = {}
+        for i in range(indeksyt[a] +1, indeksyt[b]): 
+            wartosci = (linijki[i].split())
+            chromosom = wartosci[0]
+            start = int(wartosci[1]) + 1
+            stop = int(wartosci[2]) + 1
+            wartosc = float(wartosci[3])
+            if not chromosom in sekcja:
+                sekcja[chromosom] = []
+            sekcja[chromosom].append([start, stop, wartosc])
+            
+        
+        print kolejnetracki[j] 
+        for chromosom in sekcja:
+            s=sekcja[chromosom][0][0]
+            st=int(sekcja[chromosom][1][0]) - int(sekcja[chromosom][0][0])
+            sp = int(sekcja[chromosom][0][1]) - int(sekcja[chromosom][0][0])
+            
+            declaration_line = ["fixedStep", "chrom="+chromosom, "start="+str(s), "step="+str(st), "span="+str(sp)]
+            print declaration_line
+            for linijka in sekcja[chromosom]:
+                print linijka[2]
 
         
+       
+elif jakiformat='wig':
+    a=-1
+    b=a+1
+    for j in range(0, len(indeksyt)-1):
+        a+=1
+        b+=1        
+        sekcja = {}
+        chromosom = None
+        step = 0
+        span = 0
+        start = 0
+        obecny_indeks = 0
+        for i in range(indeksyt[a] +1, indeksyt[b]):
+            linijka = linijki[i]
+            if len(linijka.strip()) == 0:
+                continue
+            if linijka.startswith('fixedStep'):
+                # dowiedz sie jaki to chromosom, jaki step, start i span
+                chromosom = 'cos'
+                step = 0
+                start = 0
+                obecny_indeks = start
+                span = 0
+                sekcja[chromosom] = []
+            else:
+                sekcja[chromosom].append([obecny_indeks, obecny_indeks + span, float(linijka)])
+                obecny_indeks += step
+            
+        #print sekcja
+        print kolejnetracki[j] #write
+        index = start
+        for chromosom in sekcja:
+            for linijka in sekcja[chromosom]:
+                print chromosom, linijka[0], linijka[1], linijka[2]
+                index += step
+                # upewnic sie, czy nie trzeba gdzies dodac albo odjac 1
+            
         
-    
-    
+          
+    #declaration_line=[]
+    #for i in range(0, len(indeksyt)-1):  
+        #declaration_line = ["fixedStep", "chrom="+chrom[i], "start="+strt[i], "step="+srep[i], "span="+sran[i]]
+        #deklaracja = " ".join(declaration_line) + "\n"
         
         
+        
+        
+        
+        
+
+    
+            
+           
     
     
+      
+
+        
+
+
